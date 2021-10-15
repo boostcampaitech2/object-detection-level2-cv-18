6d4bf2cd5e78907c890fd5e70d00f83f9d20ddcd
projectcybersyn2 <87901289+projectcybersyn2@users.noreply.github.com>
2021-10-15 12:51:31 +0900

    Add files via upload

---
+++ configs/_base_/schedules/schedule_1x.py
@@ -0,0 +1,13 @@
+# optimizer
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+
+lr_config = dict(
+    policy='CosineAnnealing',
+    warmup='linear',
+    warmup_iters=1000,
+    warmup_ratio=1.0/10,
+    min_lr_ratio=4e-6
+)
+
+runner = dict(type='EpochBasedRunner', max_epochs=36)
---
+++ configs/custom/final.py
@@ -0,0 +1,6 @@
+_base_ = [
+    'cascade_rcnn_r50_fpn.py',
+    'dataset.py',
+    'default_runtime.py',
+    'scheduler.py'
+]
